3098a19f00ec9db562b50d8ad985ea60e999665d
Regu <61461642+Regu01@users.noreply.github.com>
2023-01-16 16:03:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,47 @@
 #!/bin/bash
 
 #Install Ansible
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt update
-sudo apt -y install ansible
-ansible --version
+if [ dpkg -s ansible &> /dev/null -eq 0 ]; then
+    echo "Package ansible is installed!"
+else
+    echo "Package ansible is NOT installed!"
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+    sudo apt update
+    sudo apt -y install ansible
+    ansible --version
+fi
 
 #Install Terraform
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-wget -O- https://apt.releases.hashicorp.com/gpg | \
+if [ dpkg -s terraform &> /dev/null -eq 0 ]; then
+    echo "Package terraform is installed!"
+else
+    echo "Package terraform is NOT installed!"
+    sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+    wget -O- https://apt.releases.hashicorp.com/gpg | \
     gpg --dearmor | \
     sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-gpg --no-default-keyring \
+    gpg --no-default-keyring \
     --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
     --fingerprint
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
     https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
     sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update
-sudo apt-get -y install terraform
-touch ~/.bashrc
-terraform -install-autocomplete
-terraform -version
+    sudo apt update
+    sudo apt-get -y install terraform
+    touch ~/.bashrc
+    terraform -install-autocomplete
+    terraform -version
+fi
 
 #Qualité de vie
-apt-get -y install htop
+if [ dpkg -s htop &> /dev/null -eq 0 ]; then
+    echo "Package htop is installed!"
+else
+    echo "Package htop is NOT installed!"
+    apt-get -y install htop
+fi
+
+#Update
+sudo apt update
+
+
